bb97f11cc4454d6ee2b34789c33660a59bb4abb0
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2021-05-14 11:10:41 +0900

    chore(netlify): テキストの追加

--- src/components/page-footer/page-footer.tsx
+++ src/components/page-footer/page-footer.tsx
@@ -46,12 +46,12 @@ export class DocsPageFooter {
 
     return [
       pagination,
-      <p class="netlify">Caught a mistake or want to contribute to the documentation? <a href="https://github.com/ionic-jp/ionic-docs">Edit this on GitHub!</a> Deployed on <a href="https://www.netlify.com/">Netlify</a>.</p>,
       <div class="page-footer__row">
         {contributors.length > 0 ? <contributor-list contributors={contributors} link={contributorHref}/> : null}
         <docs-button round href={editHref}>翻訳する <ForwardArrow/></docs-button>
         {updatedText !== null ? <a class="last-updated" href={updatedHref}>Updated {updatedText}</a> : ''}
-      </div>
+      </div>,
+      <p class="netlify">Caught a mistake or want to contribute to the documentation? <a href="https://github.com/ionic-jp/ionic-docs">Edit this on GitHub!</a> Deployed on <a href="https://www.netlify.com/">Netlify</a>.</p>,
     ];
   }
 }
