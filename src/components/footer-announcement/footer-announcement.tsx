57d7c744478f4b7f37add3896d450ebaddd422a6
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2021-06-07 14:44:38 +0900

    chore(event):  https://ionic-jp.connpass.com/event/215695/ のアナウンス

--- src/components/footer-announcement/footer-announcement.tsx
+++ src/components/footer-announcement/footer-announcement.tsx
@@ -29,8 +29,8 @@ export class DocsFooterAnnouncement {
     return (
       <footer class="footer-announcement">
         <p>
-          <strong>Ionic Meetup #17 in Online:</strong>最新の情報を手に入れよう。2021/05/09(日)にオンライン開催
-          <a href="https://ionic-jp.connpass.com/event/210399/" target="_blank">
+          <strong>Ionic Meetup #18 in Online:</strong>最新の情報を手に入れよう。2021/06/26(土)にオンライン開催
+          <a href="https://ionic-jp.connpass.com/event/215695/" target="_blank">
             参加する
             <svg viewBox="0 0 512 512"><path d="M294.1 256L167 129c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.3 34 0L345 239c9.1 9.1 9.3 23.7.7 33.1L201.1 417c-4.7 4.7-10.9 7-17 7s-12.3-2.3-17-7c-9.4-9.4-9.4-24.6 0-33.9l127-127.1z"></path></svg>
           </a>
